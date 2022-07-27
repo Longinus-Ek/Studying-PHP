5dc05cabd72c77f60fa63ef8b25555f9b968e350
Erick Dias <derickbass4@gmail.com>
2022-07-27 16:52:14 -0300

    Manipulando Funções

---
+++ funcoes.php
@@ -0,0 +1,31 @@
+<?php
+
+function imprimeMensagem(string $mensagem)
+{
+    echo $mensagem . PHP_EOL;
+}
+
+function sacar(array $conta, float $valor): array
+{
+    if($conta['saldo'] < $valor){
+        imprimeMensagem("Você não pode sacar pois está sem dinheiro! Seu saldo é: " . $conta['saldo']);
+    } else{
+        $conta['saldo'] -= $valor;
+    } return $conta;
+}
+
+function depositar(array $conta, float $valor): array
+{
+    if($valor > 0){
+    $conta['saldo'] += $valor;
+    } else {
+        imprimeMensagem("O valor deve ser positivo!");
+    }
+    return $conta;
+}
+
+function titularComLetrasMaiusculas(array &$conta)
+{
+    $conta['titular'] = mb_strtoupper($conta['titular']);
+    
+}
---
+++ function_list.php
@@ -0,0 +1,5 @@
+<?php
+
+$idades = [15, 20, 30, 25, 18, 15, 48, 80, 50, 20];
+
+list($idadeErick, $idadeItala, $idadeJoão, $idadeMaria) = $nomesIdades;
