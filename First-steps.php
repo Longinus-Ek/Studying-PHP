5ea6a22d5e5bd6dd347c12c771ab05716699f79e
Erick Dias <derickbass4@gmail.com>
2022-07-27 11:40:41 -0300

    Primeiros Passosa

--- First-steps.php
+++ First-steps.php
@@ -1,3 +1,36 @@
 <?php 
+// Exibindo código no terminal
 
 echo "Olá mundo!";
+
+//Sintaxe
+
+//Declarando variáveis
+
+$idade = 25;
+
+//Operações
+
+$soma = $idade + 10;
+$div = $idade / 10;
+$mult = $idade * 10;
+$sub = $idade - 10;
+$expo = $idade ** 10; 
+$rest = $idade % 10;
+
+//Variáveis
+
+//integer
+$soma = $idade + 10;
+//Double ou float
+$div = $idade / 10;
+//String
+$texto = "Olá mundo";
+//Boolean
+$true = true;
+$false = false;
+
+//Gettype
+
+gettype($true);
+
