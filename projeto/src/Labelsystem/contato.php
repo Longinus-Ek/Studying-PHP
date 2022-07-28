f78d2b20b2e81a4f3d8bc0ca418ac8c27b4a6ea9
Erick Dias <107449926+Longinus-Ek@users.noreply.github.com>
2022-07-28 20:33:54 -0300

    Projeto Formulario

---
+++ projeto/src/Labelsystem/contato.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Labelsystem;
+
+class Contato
+{
+
+    private $email;
+
+    public function __construct(string $email)
+    {
+        $this->email = $email;
+        
+    }
+
+    public function getUsuario() : string
+    {
+        $posicao = strpos($this->email, '@');
+
+        if($posicao === false){
+            return "Usuario Inválido";
+        }
+        return substr($this->email, 0, $posicao);
+    }
+
+    public function getEmail() : string
+    {
+        return $this->email;
+    }
+
+}
